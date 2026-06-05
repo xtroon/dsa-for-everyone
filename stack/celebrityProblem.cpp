cdce8e5ea6cc03f1acfa1edb1fc893868faaa952
Ome Tiwari <omt81144@gmail.com>
2026-06-05 12:11:53 +0530

    Celebrity Problem

--- stack/celebrityProblem.cpp
+++ stack/celebrityProblem.cpp
@@ -4,9 +4,32 @@
 using namespace std;
 
 class Solution {
+    stack<int> st;
 public:
     int celebrity(vector<vector<int>>& mat) {
-        
+        int n = mat.size();
+        for(int i=0; i<n; i++) st.push(i);
+
+        while(st.size() > 1){
+            int i  = st.top();
+            st.pop();
+
+            int j = st.top();
+            st.pop();
+
+            if(mat[i][j] == 1) st.push(j);
+            else st.push(i);
+        }
+
+        int celeb = st.top();
+        for(int i=0; i<n; i++){
+            if(i == celeb) continue;
+
+            if(mat[i][celeb] == 0 ||  mat[celeb][i] == 1){
+                return -1;
+            }
+        }
+        return celeb;
     }
 };
 
