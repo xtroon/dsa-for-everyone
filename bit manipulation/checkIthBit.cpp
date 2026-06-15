e67bf1c0b14a51d91c7537ee99a528da64345e29
Ome Tiwari <omt81144@gmail.com>
2026-06-15 13:55:54 +0530

    Check ith bit

---
+++ bit manipulation/checkIthBit.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    // Brute Force: Convert to binary string - O(logN)
+    bool checkKthBitBrute(int n, int k) {
+
+        string binary = "";
+
+        while (n > 0) {
+            binary += (n % 2 ? '1' : '0');
+            n /= 2;
+        }
+
+        if (k >= binary.size())
+            return false;
+
+        return binary[k] == '1';
+    }
+
+    // Optimal: Bit Manipulation - O(1)
+    bool checkKthBitOptimal(int n, int k) {
+        return ((n >> k) & 1);
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    int n = 13;
+    int k = 2;
+
+    cout << "Brute Force : ";
+    cout << (obj.checkKthBitBrute(n, k) ? "Set" : "Not Set") << endl;
+
+    cout << "Optimal     : ";
+    cout << (obj.checkKthBitOptimal(n, k) ? "Set" : "Not Set") << endl;
+
+    return 0;
+}
