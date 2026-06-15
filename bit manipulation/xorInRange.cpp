f4fdddf80819c41449aa6e17f909a62f6c42e7c6
Ome Tiwari <omt81144@gmail.com>
2026-06-15 20:03:16 +0530

    bit amnipulation

---
+++ bit manipulation/xorInRange.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    // XOR from 1 to n
+    int xorTillN(int n) {
+
+        if (n % 4 == 0) return n;
+        if (n % 4 == 1) return 1;
+        if (n % 4 == 2) return n + 1;
+
+        return 0;
+    }
+
+    // XOR from l to r
+    int findXOR(int l, int r) {
+
+        return xorTillN(r) ^ xorTillN(l - 1);
+    }
+};
+
+int main() {
+
+    Solution obj;
+
+    int l, r;
+
+    cout << "Enter l and r: ";
+    cin >> l >> r;
+
+    cout << "XOR from " << l << " to " << r << " = "
+         << obj.findXOR(l, r) << endl;
+
+    return 0;
+}
